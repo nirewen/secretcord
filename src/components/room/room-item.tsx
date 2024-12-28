d985b3c471a74851fdfba1b84dc8616820d98591
nirewen <londero.edu@gmail.com>
2024-12-28 22:43:45 +0000

    Adjust go to room icon

--- src/components/room/room-item.tsx
+++ src/components/room/room-item.tsx
@@ -1,5 +1,5 @@
 import type { Prisma } from '@prisma/client'
-import { LogInIcon, Users2Icon } from 'lucide-react'
+import { ChevronRightIcon, Users2Icon } from 'lucide-react'
 import Link from 'next/link'
 import { Button } from '../ui/button'
 import { User } from '../user'
@@ -17,22 +17,22 @@ type Props = {
 
 export function RoomItem({ room }: Props) {
   return (
-    <div className='flex items-center justify-between gap-2'>
-      <div className='flex items-center gap-2'>
-        <span className='rounded-lg bg-neutral-800 px-2 py-1 font-mono text-2xl'>{room.id}</span>
-        <span className='flex items-center gap-2 rounded-lg bg-neutral-800 px-2 py-1'>
-          <Users2Icon className='size-4' />
-          {room.entries.length}
-        </span>
-      </div>
-      <div className='flex items-center gap-2'>
-        <User user={room.creator} />
-        <Link href={`/room/${room.id}`}>
+    <Link href={`/room/${room.id}`}>
+      <div className='flex items-center justify-between gap-2'>
+        <div className='flex items-center gap-2'>
+          <span className='rounded-lg bg-neutral-800 px-2 py-1 font-mono text-2xl'>{room.id}</span>
+          <span className='flex items-center gap-2 rounded-lg bg-neutral-800 px-2 py-1'>
+            <Users2Icon className='size-4' />
+            {room.entries.length}
+          </span>
+        </div>
+        <div className='flex items-center gap-2'>
+          <User user={room.creator} />
           <Button variant='secondary' size='icon'>
-            <LogInIcon />
+            <ChevronRightIcon />
           </Button>
-        </Link>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
